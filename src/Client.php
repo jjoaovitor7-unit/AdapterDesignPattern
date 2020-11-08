3b9a7118f0efe6e2efdb67d80125d8947db9eda0
João Vítor <joao.sferreira@souunit.com.br>
2020-11-08 17:43:31 -0300

    seleção de eletrodomésticos.

--- src/Client.php
+++ src/Client.php
@@ -12,58 +12,89 @@ require_once(__DIR__ . "/model/eletrodomesticos/BatedeiraAdapter.php");
 require_once(__DIR__ . "/model/eletrodomesticos/Batedeira.php");
 require_once(__DIR__ . "/model/eletrodomesticos/CafeteiraAdapter.php");
 require_once(__DIR__ . "/model/eletrodomesticos/Cafeteira.php");
+require_once(__DIR__ . "/model/eletrodomesticos/TorradeiraAdapter.php");
+require_once(__DIR__ . "/model/eletrodomesticos/Torradeira.php");
 
 class Client {
     public function main()
     {
         echo "Produtos";
-        echo "\n1-Liquidificador";
-        echo "\n2-Fogão";
-        echo "\n3-Geladeira";
-        echo "\n4-Ar-Condicionado";
-        echo "\n5-Batedeira";
-        echo "\n6-Cafeteira";
+        echo "\n1-Eletrodomésticos";
+        echo "\n2-Eletrodomésticos Linha Branca";
         echo "\n:";
-
         $optionProdutos = fgets(STDIN);
-        if ($optionProdutos == 1)
-        {
-            $produto = new LiquidificadorAdapter(new Liquidificador());
-            $produto->execute();
+
+        if ($optionProdutos == 1) {
+            echo "\nEletrodomésticos";
+            echo "\n1-Batedeira";
+            echo "\n2-Cafeteira";
+            echo "\n3-Liquidificador";
+            echo "\n4-Torradeira";
+            echo "\n:";
+
+            $optionEletrodomesticos = fgets(STDIN);
+            if ($optionEletrodomesticos == 1)
+            {
+                $produto = new BatedeiraAdapter(new Batedeira());
+                $produto->execute();
+            }
+
+            else if ($optionEletrodomesticos == 2)
+            {
+                $produto = new CafeteiraAdapter(new Cafeteira());
+                $produto->execute();
+            }
+
+            else if ($optionEletrodomesticos == 3)
+            {
+                $produto = new LiquidificadorAdapter(new Liquidificador());
+                $produto->execute();
+            }
+
+            else if ($optionEletrodomesticos == 4)
+            {
+                $produto = new TorradeiraAdapter(new Torradeira());
+                $produto->execute();
+            }
+
+            else {
+                echo "Opção não encontrada.\n";
+            }
         }
 
-        else if ($optionProdutos == 2)
-        {
-            $produto = new FogaoAdapter(new Fogao());
-            $produto->execute();
-        }
+        else if ($optionProdutos == 2){
+            echo "\nEletromésticos Linha Branca";
+            echo "\n1-ArCondicionado";
+            echo "\n2-Fogão";
+            echo "\n3-Geladeira";
+            echo "\n:";
 
-        else if ($optionProdutos == 3)
-        {
-            $produto = new GeladeiraAdapter(new Geladeira());
-            $produto->execute();
-        }
+            $optionEletrodomesticos = fgets(STDIN);
+            if ($optionEletrodomesticos == 1)
+            {
+                $produto = new ArCondicionadoAdapter(new ArCondicionado());
+                $produto->execute();
+            }
 
-        else if ($optionProdutos == 4)
-        {
-            $produto = new ArCondicionadoAdapter(new ArCondicionado());
-            $produto->execute();
-        }
+            else if ($optionEletrodomesticos == 2)
+            {
+                $produto = new FogaoAdapter(new Fogao());
+                $produto->execute();
+            }
 
-        else if ($optionProdutos == 5)
-        {
-            $produto = new BatedeiraAdapter(new Batedeira());
-            $produto->execute();
+            else if ($optionEletrodomesticos == 3)
+            {
+                $produto = new GeladeiraAdapter(new Geladeira());
+                $produto->execute();                
+            }
+            
+            else {
+                echo "Opção não encontrada.\n";
+            }
         }
-
-        else if ($optionProdutos == 6)
-        {
-            $produto = new CafeteiraAdapter(new Cafeteira());
-            $produto->execute();
-        }
-
+        
         else {
-            echo "Produto não encontrado.\n";
+            echo "Opção não encontrada.\n";
         }
     }
 }
